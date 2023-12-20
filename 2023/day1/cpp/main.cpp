771fdeaa3ad5cb90fdb333653b6792e378e8fa35
Marius Eskedal <marius.eskedal@gmail.com>
2023-12-20 21:25:10 +0100

    AoC 2023 Day 1 cpp properly done

--- 2023/day1/cpp/main.cpp
+++ 2023/day1/cpp/main.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <fstream>
 #include <iostream>
 #include <numeric>
@@ -6,26 +7,47 @@
 #include <regex>
 #include <map>
 
-std::map<std::string, std::string> digit_word_to_digit_char = {
-    {"one", "1"},
-    {"two", "2"},
-    {"three", "3"},
-    {"four", "4"},
-    {"five", "5"},
-    {"six", "6"},
-    {"seven", "7"},
-    {"eight", "8"},
-    {"nine", "9"},
-    {"1", "1"},
-    {"2", "2"},
-    {"3", "3"},
-    {"4", "4"},
-    {"5", "5"},
-    {"6", "6"},
-    {"7", "7"},
-    {"8", "8"},
-    {"9", "9"}
-};
+
+/**
+ * @brief Extracts the two digit number from a string as described in the problem of Day1 2023. 
+ * Regex pattern matches a digit word (one, two, three, etc) or a digit (1, 2, 3, etc) and then optionally matches another digit word or digit.
+ * @param str The string to extract the number from.
+ * @return The integer two digit number.
+ */
+int extract_number(const std::string &str) {
+    const static std::map<std::string, std::string> digit_word_to_digit_char = {
+        {"one", "1"},
+        {"two", "2"},
+        {"three", "3"},
+        {"four", "4"},
+        {"five", "5"},
+        {"six", "6"},
+        {"seven", "7"},
+        {"eight", "8"},
+        {"nine", "9"},
+        {"1", "1"},
+        {"2", "2"},
+        {"3", "3"},
+        {"4", "4"},
+        {"5", "5"},
+        {"6", "6"},
+        {"7", "7"},
+        {"8", "8"},
+        {"9", "9"}
+    };
+    const std::regex pattern("(one|two|three|four|five|six|seven|eight|nine|\\d)(?:.*(one|two|three|four|five|six|seven|eight|nine|\\d))?", std::regex::icase);
+    std::smatch match;
+    if (!std::regex_search(str, match, pattern)) {
+        throw std::runtime_error("Could not match " + str);
+    }
+    std::string first_digit = match.str(1);
+    std::string last_digit = match.str(2);
+    if (last_digit == "") {
+        last_digit = first_digit;
+    }
+    std::string combined_number = digit_word_to_digit_char.at(first_digit) + digit_word_to_digit_char.at(last_digit);
+    return std::stoi(combined_number);
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -38,38 +60,13 @@ int main(int argc, char* argv[]) {
         std::cerr << "Could not open file: " << argv[1] << std::endl;
         return 1;
     }
-    const std::regex pattern("(one|two|three|four|five|six|seven|eight|nine|\\d)(?:.*(one|two|three|four|five|six|seven|eight|nine|\\d))?.*");
-    // const std::regex first_pattern("(one|two|three|four|five|six|seven|eight|nine|\\d)", std::regex::icase);
-    // const std::regex last_pattern(".*(one|two|three|four|five|six|seven|eight|nine|\\d).*?$", std::regex::icase);
-
-    auto regex_match = [pattern](auto &s) { 
-        std::smatch match;
-        std::smatch last_match;
-        std::regex_search(s, match, pattern);
-        if (match.size() < 2) {
-            std::cerr << "No matched group " << s << std::endl;
-            return 1;
-        }
-
-        std::string first_digit = match.str(1);
-        std::string last_digit = match.size() == 2 ? match.str(2) : match.str(3);
-
-        // if (!std::regex_search(s, last_match, last_pattern)) {
-        //     std::cerr << "Could not find last match in: " << s << std::endl;
-        //     return 1;
-        // }
-
-        // std::string first_digit = match.str(1);
-        // std::string last_digit = last_match.str(1);
-        std::string combined_number = digit_word_to_digit_char[first_digit] + digit_word_to_digit_char[last_digit];
-        return std::stoi(combined_number);
-    };
 
     int sum = 0;
 
-    for (const auto &result : std::ranges::istream_view<std::string>(input) | std::views::transform([regex_match](auto &s) { return regex_match(s);})) {
+    for (const auto &result : std::ranges::istream_view<std::string>(input) | std::views::transform([](auto &s) { return extract_number(s);})) {
         sum += result;
     }
 
     std::cout << "Sum: " << sum << std::endl;
+    return 0;
 }
