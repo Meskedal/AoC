b5698d03743300af3a99f2b02a944f04beea13de
Marius Eskedal <marius.eskedal@gmail.com>
2025-12-09 21:21:48 +0100

    Added day7 part 1

--- 2025/rust/day7/src/lib.rs
+++ 2025/rust/day7/src/lib.rs
@@ -1,9 +1,38 @@
+use std::collections::HashSet;
+
+
 pub fn input_generator(input: &str) -> Vec<Vec<char>> {
 	input.lines().map(|l| l.chars().collect()).collect()
 }
 
 pub fn part_1(input: &Vec<Vec<char>>) -> i32 {
-	0
+	let rows_max = input.len();
+	let cols_max = input[0].len();
+
+	let mut matrix = input.clone();
+	let mut splitters = HashSet::new();
+	for row in 0..rows_max-1 {
+		for col in 0..cols_max {
+			let c = matrix[row][col];
+			if c != 'S' && c != '|' {
+				continue;
+			}
+			let c_below = matrix[row+1][col];
+			if c_below != '^' {
+				matrix[row+1][col] = '|';
+				continue;	
+			}
+			// Could alternatively just overwrite the splitter at this point
+			splitters.insert((row+1, col));
+			if col+1 < cols_max {
+				matrix[row+1][col+1] = '|';
+			}
+			if col-1 > 0 {
+				matrix[row+1][col-1] = '|';
+			}
+		}
+	}
+	splitters.len() as i32
 }
 
 pub fn part_2(input: &Vec<Vec<char>>) -> i32 {
--- 2025/rust/day7/tests/test.rs
+++ 2025/rust/day7/tests/test.rs
@@ -9,7 +9,7 @@ fn test_day_7_part_1_example() {
 #[test]
 fn test_day_7_part_2_example() {
 	let input = day7::input_generator(include_str!("../input/example.txt"));
-	let expected = 0;
+	let expected = 40;
 	let result = day7::part_2(&input);
 	assert_eq!(result, expected);
 }
