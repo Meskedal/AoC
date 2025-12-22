c9e8ffdc3f3e91a5474aab3fd322358c182dc06b
Marius Eskedal <marius.eskedal@gmail.com>
2025-12-22 12:56:34 +0100

    Added day 10 progress

--- 2025/rust/day10/src/lib.rs
+++ 2025/rust/day10/src/lib.rs
@@ -48,14 +48,54 @@ pub fn input_generator(input: &str) -> (Vec<i32>, Vec<Vec<Vec<i32>>>, Vec<Vec<i3
     )
 }
 
+fn xor(indicator_light: i32, button: Vec<i32>) -> i32 {
+	let mut il = indicator_light;
+	for b in button {
+		il ^= 1 << b;
+	}
+	il
+}
+
+fn recursive(bits: i32, indicator_light: i32, buttons: Vec<Vec<i32>>, depth: i32, lowest_candidate: i32) -> Option<i32> {
+    if depth >= lowest_candidate {
+        return None;
+    }
+    
+    let mut new_lowest_candidate = lowest_candidate;
+    
+    for b in &buttons {
+        let indicator_light_candidate = xor(bits, b.clone());
+        
+        if indicator_light_candidate == indicator_light {
+            new_lowest_candidate = new_lowest_candidate.min(depth);
+            continue;
+        }
+        
+        if let Some(result) = recursive(indicator_light_candidate, indicator_light, buttons.clone(), depth + 1, new_lowest_candidate) {
+            new_lowest_candidate = new_lowest_candidate.min(result);
+        }
+    }
+    
+    if new_lowest_candidate < lowest_candidate {
+        Some(new_lowest_candidate)
+    } else {
+        None
+    }
+}
+
 pub fn part_1(input: &(Vec<i32>, Vec<Vec<Vec<i32>>>, Vec<Vec<i32>>)) -> i32 {
 	let (indicator_lights, buttons, joltage) = input;
+	let mut sum = 0;
 	for i in 0..indicator_lights.len() {
 		let buttons = &buttons[i];
 		let indicator_light = indicator_lights[i];
 		// Now the actual part 1
+		let mut bits = 0;
+		let result = recursive(bits, indicator_light, buttons.clone(), 1, 10).unwrap();
+        println!("{:?}", result);
+        sum += result;
 	}
-	0
+	sum
 }
 
 pub fn part_2(input: &(Vec<i32>, Vec<Vec<Vec<i32>>>, Vec<Vec<i32>>)) -> i32 {
